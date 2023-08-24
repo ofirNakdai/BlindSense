53dfc6190e11cb04f21d915945f85e857d2d4ae0
ofirNakdai <93441082+ofirNakdai@users.noreply.github.com>
2023-08-24 16:29:19 +0300

    adding registry Project

---
+++ BlindSenseRegistry/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace BlindSenseRegistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtClientName = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNum = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(181, 361);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(210, 53);
+            this.submitBtn.TabIndex = 0;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chip ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contact Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Contact phone number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Contact E-mail:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Client Name:";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(224, 125);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(293, 22);
+            this.txtid.TabIndex = 2;
+            // 
+            // txtClientName
+            // 
+            this.txtClientName.Location = new System.Drawing.Point(224, 173);
+            this.txtClientName.Name = "txtClientName";
+            this.txtClientName.Size = new System.Drawing.Size(293, 22);
+            this.txtClientName.TabIndex = 2;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(224, 219);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(293, 22);
+            this.txtContactName.TabIndex = 2;
+            // 
+            // txtPhoneNum
+            // 
+            this.txtPhoneNum.Location = new System.Drawing.Point(224, 262);
+            this.txtPhoneNum.Name = "txtPhoneNum";
+            this.txtPhoneNum.Size = new System.Drawing.Size(293, 22);
+            this.txtPhoneNum.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(224, 304);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(293, 22);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label6.Location = new System.Drawing.Point(136, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(310, 37);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Blind Sense Registry";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 450);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhoneNum);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtClientName);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submitBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtClientName;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtPhoneNum;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BlindSenseRegistry/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+
+
+namespace BlindSenseRegistry
+{
+    public partial class Form1 : Form
+    {
+        private string ApiUrl = "http://54.173.52.224:3011";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void submitBtn_Click(object sender, EventArgs e)
+        {
+            string name = Uri.EscapeDataString(txtClientName.Text);
+            string contactName = Uri.EscapeDataString(txtContactName.Text);
+            string contactPhone = Uri.EscapeDataString(txtPhoneNum.Text);
+            string contactEmail = Uri.EscapeDataString(txtEmail.Text);
+            string chipID = Uri.EscapeDataString(txtid.Text);
+
+            using (HttpClient client = new HttpClient())
+            {
+                string apiUrl = $"{ApiUrl}/register?clientName={name}&contactName={contactName}&contactPhone={contactPhone}&contactEmail={contactEmail}&clientID={chipID}";
+                HttpResponseMessage response = await client.PostAsync(apiUrl, null);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Data submitted successfully!");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to submit data.");
+                }
+            }
+        }
+    }
+}
